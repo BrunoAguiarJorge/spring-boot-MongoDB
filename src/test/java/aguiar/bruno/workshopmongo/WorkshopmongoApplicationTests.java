a48975775e6b9d68e9a17d7a7808832e1f80e1d4
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2021-06-02 21:05:57 +0100

    Project Created

---
+++ src/test/java/aguiar/bruno/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package aguiar.bruno.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
